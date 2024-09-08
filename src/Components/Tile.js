117b8a275743a74fefcdddf6f8424681fe255d9e
Ben gucha <36176354-mrprabhatdwived@users.noreply.replit.com>
2024-09-08 09:08:22 +0000

    Commit from repl

--- src/Components/Tile.js
+++ src/Components/Tile.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+
 export default function Tile() {
-  const [gridIndices, setGridIndices] = useState(Array.from({ length: 64 }, (_, index) => index));
+  const [gridIndices] = useState(Array.from({ length: 64 }, (_, index) => index));
 
   const returnRowNum = (index) => {
     return Math.floor(index / 8);
@@ -8,11 +9,11 @@ export default function Tile() {
 
   return (
     <div className="h-screen bg-slate-900 flex justify-center items-center">
-      <div className='grid grid-cols-8 gap-0 w-3/5'>
+      <div className="grid grid-cols-8 gap-0" style={{ width: '100vw', maxWidth: '90vh', height: 'auto' }}>
         {gridIndices.map((index) => (
           <div
             key={index}
-            className={`h-20 w-20 ${(index + returnRowNum(index)) % 2 === 0 ? "bg-white" : "bg-slate-600"}`}
+            className={`h-auto aspect-square ${(index + returnRowNum(index)) % 2 === 0 ? "bg-white" : "bg-slate-600"}`}
           ></div>
         ))}
       </div>
